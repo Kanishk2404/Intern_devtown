fb149b24d44e0c322e194804209ce8c1757ab39f
Kanishk2404 <114097334+Kanishk2404@users.noreply.github.com>
2023-07-20 23:14:13 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const NavigationBar = () => {
   return (
-    <nav className="navigation-bar">
-      {/* Add navigation links here */}
+    <nav className="bg-blue-500 p-4 text-white flex justify-between">
+      <h1 className="text-2xl font-bold">Notes App</h1>
+      <div className="space-x-2">
+        <button className="px-3 py-1 rounded bg-blue-600 hover:bg-blue-700">
+          New Note
+        </button>
+        <button className="px-3 py-1 rounded bg-blue-600 hover:bg-blue-700">
+          Settings
+        </button>
+      </div>
     </nav>
   );
 };
