6a44cc83df827e726bb8cc57938e0f0ec69cfa5b
Kanishk2404 <114097334+Kanishk2404@users.noreply.github.com>
2023-07-20 23:13:31 +0530

    Update edito.js

--- edito.js
+++ edito.js
@@ -1,29 +1,55 @@
-import React, { useState } from 'react';
+// NoteEditor.js
+import React, { useState, useEffect } from 'react';
 
-const NoteEditor = ({ onSave }) => {
+const NoteEditor = ({ notes, onSave, editIndex, onEditNote }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  // When editIndex changes, update the input fields with the corresponding note data
+  useEffect(() => {
+    if (editIndex !== null) {
+      const { title: editTitle, content: editContent } = notes[editIndex];
+      setTitle(editTitle);
+      setContent(editContent);
+    } else {
+      setTitle('');
+      setContent('');
+    }
+  }, [editIndex, notes]);
+
   const handleSave = () => {
-    onSave({ title, content });
+    if (editIndex !== null) {
+      // Edit existing note
+      onEditNote(editIndex, { title, content });
+    } else {
+      // Add new note
+      onSave({ title, content });
+    }
     setTitle('');
     setContent('');
   };
 
   return (
-    <div className="note-editor">
+    <div className="bg-white shadow-lg rounded-lg p-4">
       <input
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Title"
+        className="border p-2 mb-2 rounded w-full text-blue-800 placeholder-blue-200"
       />
       <textarea
         value={content}
         onChange={(e) => setContent(e.target.value)}
-        placeholder="Write your note here..."
+        placeholder="Add your note here..."
+        className="border p-2 rounded w-full h-32 text-blue-800 placeholder-blue-200"
       />
-      <button onClick={handleSave}>Save Note</button>
+      <button
+        onClick={handleSave}
+        className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-4 float-right"
+      >
+        {editIndex !== null ? 'Save Edit' : 'Save Note'}
+      </button>
     </div>
   );
 };
